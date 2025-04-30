8c7f4cea39d9c961a076db02e127709b9bc70d7e
Chris Mikelson <chrismikelson@gmail.com>
2025-04-29 22:46:29 -0500

    organize candle data

---
+++ data/candles/index.ts
@@ -0,0 +1,15 @@
+import decisiveCandle from './decisiveCandle.json';
+import equalOpenCloseCandle from './equalOpenCloseCandle.json';
+import explosiveCandle from './explosiveCandle.json';
+import indecisiveCandle from './indecisiveCandle.json';
+import negativeCandle from './negativeCandle.json';
+import zeroRangeCandle from './zeroRangeCandle.json';
+
+export { 
+    decisiveCandle, 
+    equalOpenCloseCandle, 
+    explosiveCandle, 
+    indecisiveCandle, 
+    negativeCandle, 
+    zeroRangeCandle 
+};
--- data/index.ts
+++ data/index.ts
@@ -1,8 +1,1 @@
-import decisiveCandle from './decisiveCandle.json';
-import equalOpenCloseCandle from './equalOpenCloseCandle.json';
-import explosiveCandle from './explosiveCandle.json';
-import indecisiveCandle from './indecisiveCandle.json';
-import negativeCandle from './negativeCandle.json';
-import zeroRangeCandle from './zeroRangeCandle.json';
-
-export { decisiveCandle, equalOpenCloseCandle, explosiveCandle, indecisiveCandle, negativeCandle, zeroRangeCandle };
+export * from './candles';
