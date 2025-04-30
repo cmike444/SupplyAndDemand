c3f1cde00067a91189d429b68207c4ae077de2d6
Chris Mikelson <chrismikelson@gmail.com>
2025-04-29 22:26:30 -0500

    add jest

---
+++ data/index.ts
@@ -0,0 +1,5 @@
+import decisiveCandle from './decisiveCandle.json';
+import indecisiveCandle from './indecisiveCandle.json';
+import explosiveCandle from './explosiveCandle.json';
+
+export { decisiveCandle, indecisiveCandle, explosiveCandle };
---
+++ test/isDecisiveCandle.test.ts
@@ -0,0 +1,23 @@
+import { isDecisiveCandle } from '../lib/isDecisiveCandle';
+import { explosiveCandle, decisiveCandle, indecisiveCandle } from '../data';
+
+
+test('returns true for a decisive bullish candle with significant difference', () => {
+	expect(isDecisiveCandle(decisiveCandle)).toBe(true);
+});
+
+test('returns true for a decisive bearish candle with significant difference', () => {
+	expect(isDecisiveCandle(decisiveCandle)).toBe(true);
+});
+
+test('returns true for a explosive bullish candle with significant difference', () => {
+	expect(isDecisiveCandle(explosiveCandle)).toBe(true);
+});
+
+test('returns false for a candle with small difference between open and close', () => {
+	expect(isDecisiveCandle(indecisiveCandle)).toBe(false);
+});
+
+test('returns false for a candle with equal open and close', () => {
+	expect(isDecisiveCandle(indecisiveCandle)).toBe(false);
+});
